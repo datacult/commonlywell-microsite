eb2ee524addbf2a67f11632d08bc6d2a58fe1148
AminaBrown <amina@datacult.com>
2022-08-11 16:20:18 -0400

    update DOM order

--- comparison.js
+++ comparison.js
@@ -640,6 +640,8 @@ let comparison = ((data, data_map = {x:'x_value', y:'y_value', group:'step_value
 
     
         comp_image.attr('y',yScale(0)-document.getElementById('binary-comparison').getBBox().height);
+        document.getElementById('comparison-group').insertBefore(document.getElementById('binary-comparison'), document.getElementById('x-axis-comparison'));
+        
 
     //draw highlights
     var dipstat = d3.group(data, d => d[data_map.group]);
